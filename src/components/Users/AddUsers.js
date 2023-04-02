b2d02fc62ebae7ce3ce863ca56627611a304b2e5
jo-duchan <jo_duchan@icloud.com>
2023-04-03 01:29:05 +0900

    section-102

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "components/UI/Card";
 import Button from "components/UI/Button";
 import classes from "components/Users/AddUsers.module.css";
 
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const AgeChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={AgeChangeHandler}
+        />
         <Button type="submit">Add Users</Button>
       </form>
     </Card>
