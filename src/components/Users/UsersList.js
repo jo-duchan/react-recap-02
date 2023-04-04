23495b47c42c17fee27d9ca222222ffb1e579071
jo-duchan <jo_duchan@icloud.com>
2023-04-05 00:27:51 +0900

    section8-104

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "components/UI/Card";
+import classes from "components/Users/UsersList.module.css";
+
+function UsersList({ users }) {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default UsersList;
